e4e23adea1cc2efb5e0caaf1d105741fa010ef4f
Il23555 <48441714+Il23555@users.noreply.github.com>
2019-06-06 22:31:12 +0300

    Add files via upload

---
+++ Program_10/Program.fs
@@ -0,0 +1,11 @@
+﻿type TimeOfDay = { hours: int; minutes: int; f: string }
+
+let (.>.) x y = 
+    if x.f = y.f then 
+        if x.hours = y.hours then
+            if x.minutes > y.minutes then true 
+            else false
+        elif x.hours > y.hours then true else false
+     elif x.f = "pm" then true else false
+
+

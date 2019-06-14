7054eca56a3dd56a7bf214362fa984b6bc6889b9
Il23555 <48441714+Il23555@users.noreply.github.com>
2019-06-14 16:33:38 +0500

    Add files via upload

--- Program_14/Program.fs
+++ Program_14/Program.fs
@@ -1,21 +1,18 @@
 ﻿// 40.1
-let rec summa (xs,p,s) = 
+let rec sum (p, xs) = 
     match xs with
-    |head :: tail when p head -> summa(tail,p,s + head)
-    |head :: tail -> summa(tail,p,s)
-    |_ -> s
-
-let rec sum (p, xs) = summa(xs,p,0)
+    |[head] when p head -> head
+    |head :: tail when p head -> head + sum(p, tail)
+    |_ :: tail -> sum(p, tail)
+    |_ -> 0
 
 // 40.2.1
-let rec count1 (x,xs,i) =
+let rec count (xs, n) =
     match xs with
-    |head :: tail when head = x -> count1(x,tail,i+1)
-    |head :: tail when head > x -> i
-    |head :: tail -> count1(x,tail,i)
-    |_ -> i
-
-let rec count (xs, n) = count1(n,xs,0)
+    |head :: tail when head = n -> 1 + count(tail,n)
+    |head :: tail when head > n -> 0
+    |head :: tail -> count(tail,n)
+    |_ -> 0
 
 // 40.2.2
 let rec insert (xs, n) = 
@@ -26,24 +23,22 @@ let rec insert (xs, n) =
     |_ -> [n]
 
 // 40.2.3
-let rec intersect1 (xs1,xs2,list) =
-    match (xs1,xs2) with
-    |(head1 :: tail1, head2 :: tail2) when head1 = head2 -> head1 :: intersect1 (tail1, tail2, list)
-    |(head1 :: tail1, head2 :: tail2) when head1 < head2 -> intersect1 (tail1, head2 :: tail2, list)
-    |(head1 :: tail1, head2 :: tail2) when head1 > head2 -> intersect1 (head1 :: tail1, tail2, list)
-    |_ -> list
-
-let rec intersect (xs1, xs2) = intersect1(xs1,xs2,[])
+let rec intersect (xs1, xs2) =
+     match (xs1,xs2) with
+    |(head1 :: tail1, head2 :: tail2) when head1 = head2 -> head1 :: intersect (tail1, tail2)
+    |(head1 :: tail1, head2 :: tail2) when head1 < head2 -> intersect (tail1, head2 :: tail2)
+    |(head1 :: tail1, head2 :: tail2) when head1 > head2 -> intersect (head1 :: tail1, tail2)
+    |_ -> [] @ xs2
 
 // 40.2.4
-let rec plus1 (xs1,xs2,list)=
+let rec plus (xs1,xs2)=
      match (xs1,xs2) with
-     |(head1 :: tail1, head2 :: tail2) when head1 = head2 -> head1 :: plus1 (tail1, tail2, list)
-     |(head1 :: tail1, head2 :: tail2) when head1 < head2 -> head1 :: plus1 (tail1, head2 :: tail2, list)
-     |(head1 :: tail1, head2 :: tail2) when head1 > head2 -> head2 :: plus1 (head1 :: tail1, tail2, list)
-     |_ -> list
-
-let rec plus (xs1, xs2) = plus1(xs1,xs2,[])
+     |(head1 :: tail1, head2 :: tail2) when head1 = head2 -> head1 :: head2:: plus (tail1, tail2)
+     |(head1 :: tail1, head2 :: tail2) when head1 < head2 -> head1 :: plus (tail1, head2 :: tail2)
+     |(head1 :: tail1, head2 :: tail2) when head1 > head2 -> head2 :: plus (head1 :: tail1, tail2)
+     | (_, []) -> xs1
+     | ([], xs2) -> xs2
+     |_ -> []
 
 // 40.2.5
 let rec minus_1 (xs1,xs2,list) =
@@ -52,7 +47,7 @@ let rec minus_1 (xs1,xs2,list) =
      |(head1 :: tail1, head2 :: tail2) when head1 < head2 -> head1 :: minus_1 (tail1, head2 :: tail2, list)
      |_ -> list @ xs1
 
-let rec minus (xs1, xs2) = minus_1(xs1,intersect(xs1,xs2),[])
+let rec minus(xs1, xs2) = minus_1(xs1,intersect(xs1,xs2),[])
 
 // 40.3.1
 let rec smallest = 
@@ -87,5 +82,27 @@ let rec revrev =
     let rec rev = function
         |head :: tail -> List.rev head ::  rev tail
         |_ -> []
-    fun xs  -> rev (List.rev xs)
+    fun xs -> rev (List.rev xs)
 
+
+(*let f x = x > 5
+let l1 = [1;2;3;4;5;6;7]
+let l2 = [1]
+let l0 = [1]
+let l3 = [3;4;5]
+let l6 = [3;6;6;7;8]
+let l10 = [[3;4;5];[3;5];[4;5]]
+let l4 = [10;4;6;3;1]
+
+*)
+  
+//printf "%s" (string (plus(l2,l0)))
+//printf "%s" (string (sum(f,l6)))
+//printf "%s" (string (count(l6,6)))
+//printf "%s" (string (insert(l3,3)))
+//printf "%s" (string (intersect(l1,l3)))
+//printf "%s" (string (minus(l1,l3)))
+//printf "%s" (string (smallest(l4)))
+//printf "%s" (string (delete(6,l6)))
+//printf "%s" (string (sort (l4)))
+//printf "%s" (string (revrev(l10)))

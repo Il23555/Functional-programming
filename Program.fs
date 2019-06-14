42a67524a14183d04fec2271e9caa8466697e366
Il23555 <48441714+Il23555@users.noreply.github.com>
2019-06-14 22:44:34 +0500

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿// 41.4.1
+let list_filter1 f xs = List.foldBack (fun head tail -> if f head then head :: tail else tail) xs []
+
+// 41.4.2
+let sum (p, xs) = List.foldBack (fun head tail -> if p head then head + tail else tail) xs 0
+
+// 41.4.3
+let revrev = fun list -> List.fold (fun head tail -> List.fold(fun head1 tail1 -> tail1 :: head1) [] tail :: head) [] list

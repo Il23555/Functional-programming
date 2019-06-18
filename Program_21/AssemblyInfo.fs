bfce55651940df03fa96824ca069c617a73eecba
Il23555 <48441714+Il23555@users.noreply.github.com>
2019-06-18 11:58:33 +0500

    Add files via upload

---
+++ Program_21/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Program_21.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Program_21")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Program_21")>]
+[<assembly: AssemblyCopyright("Copyright ©  2019")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("eb275eae-70bc-4cb9-9469-a8adaf10d2aa")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Program_21/Program.fs
@@ -0,0 +1,14 @@
+﻿// 50.2.1
+let fac_seq = seq {
+     let mutable p = 1
+     yield p
+     for i in 1..1000 do 
+        p <- p*i
+        yield p }
+    
+// 50.2.2
+let seq_seq = seq {
+    yield 0
+    for i in 1..1000 do
+        yield (0-i)
+        yield i }
